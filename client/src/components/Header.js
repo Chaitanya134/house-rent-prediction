0346de6d77722e855dd60e448ed163afb0f25f14
Chaitanya134 <chaitanyagiri2@gmail.com>
2022-09-03 15:21:48 +0530

    Created Basic UI

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h2 className='logo'>House Rent Predictor</h2>
+        <button>Predict</button>
+    </header>
+  )
+}
+
+export default Header
---
+++ client/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='hero-section'>
+        <div>
+            <h1>Predict House Rent</h1>
+            <p>Get the approximate house rent based on some attributes.</p>
+            <button>Predict</button>
+        </div>
+        <img src="/house.png" alt='house' />
+    </section>
+  )
+}
+
+export default Hero
